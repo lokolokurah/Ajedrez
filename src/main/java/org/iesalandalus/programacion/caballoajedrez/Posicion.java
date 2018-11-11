a3f0b18679f6f3b21e6722a5a8a8cdcc60f38fd7
Jaime Ruiz Ramirez <Jaime@HYRULE-5500X>
2018-11-11 21:28:43 +0100

    Creado constructo.
    Comentario sobre 2 posibilidades a la hora de relizarlo llamando al 
    metodo set de ambos.

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -5,6 +5,8 @@
  */
 package org.iesalandalus.programacion.caballoajedrez;
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 /**
  *
  * @author Jaime
@@ -46,4 +48,32 @@ public class Posicion
     }
     
     
+    //Creado el constructor, usare el Constructor que inicializa todos los atributos a través de parámetros
+    public Posicion(int fila, char columna){
+				
+                                        //Comprobamos validez de datos
+                                        if (fila > 0 && fila < 9) {
+                                                this.fila=fila;
+					}
+					else {
+						throw new IllegalArgumentException("Los valores de la fila son erroneos");
+					}
+					if (columna == 'a' || columna == 'b' || columna == 'c' || columna == 'd' || columna == 'e' || columna == 'f' || columna == 'g' || columna == 'h'){
+                                                this.columna=columna;
+					}
+					else {
+						throw new IllegalArgumentException("Los valores de la columna son erroneos");
+					}
+                                        /*
+                                        También hay otra forma:
+                                        - Llamamos al método que permite modificar la fila ya que 
+        en dicho método validamos si la fila es o no correcta:
+                                        setFila(fila); 
+                                        Lo mismo para columna:
+                                        setColumna(columna);
+                                        */
+                                      
+				
+ 				
+    }
 }
