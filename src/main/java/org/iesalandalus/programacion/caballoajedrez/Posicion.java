cc5477322bf926734956af0c3782e1ff27391093
Jaime Ruiz Ramirez <Jaime@HYRULE-5500X>
2018-11-12 12:38:27 +0100

    Simplificado el constructor de validación + 8. Constructor Copia

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -49,31 +49,19 @@ public class Posicion
     
     
     //Creado el constructor, usare el Constructor que inicializa todos los atributos a través de parámetros
-    public Posicion(int fila, char columna){
-				
-                                        //Comprobamos validez de datos
-                                        if (fila > 0 && fila < 9) {
-                                                this.fila=fila;
-					}
-					else {
-						throw new IllegalArgumentException("Los valores de la fila son erroneos");
-					}
-					if (columna == 'a' || columna == 'b' || columna == 'c' || columna == 'd' || columna == 'e' || columna == 'f' || columna == 'g' || columna == 'h'){
-                                                this.columna=columna;
-					}
-					else {
-						throw new IllegalArgumentException("Los valores de la columna son erroneos");
-					}
-                                        /*
-                                        También hay otra forma:
-                                        - Llamamos al método que permite modificar la fila ya que 
-        en dicho método validamos si la fila es o no correcta:
-                                        setFila(fila); 
-                                        Lo mismo para columna:
-                                        setColumna(columna);
-                                        */
-                                      
-				
- 				
+    public Posicion(int fila, char columna)
+    {	
+        //Comprobamos validez de datos
+        //Cambiada a forma más simplificada llamando a metodos get y set
+        setFila(fila);
+        setColumna(columna);                            
+    }
+    
+    
+    //Creado el constructor copia para la clase Posicion
+    public Posicion(Posicion copiaPosicion)
+    {
+        this.fila=copiaPosicion.fila;
+        this.columna=copiaPosicion.columna;
     }
 }
